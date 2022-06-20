7738437fdb84df05e3ea4adcb3b6162c65945b54
rocha19 <rochaarqueologia@outlook.com>
2022-06-20 13:09:22 -0300

    test: remove dollar side-effects

--- test/use-cases/dollar.spec.ts
+++ test/use-cases/dollar.spec.ts
@@ -3,7 +3,9 @@ import { Dollar } from '@/use-cases';
 describe('Dollar', () => {
   it('Should return 10 when five dollars are multiplied by two', () => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).toEqual(10);
+    let product: Dollar = five.times(2);
+    expect(product.amount).toEqual(10);
+    product = five.times(3);
+    expect(product.amount).toEqual(15);
   });
 });
