cfb26e90a0c52cddff803cb6d3d1fb6892d3932c
rocha19 <rochaarqueologia@outlook.com>
2022-06-20 14:10:14 -0300

    refactor: description test

--- test/use-cases/money.spec.ts
+++ test/use-cases/money.spec.ts
@@ -1,17 +1,17 @@
 import { Dollar, Euro } from '@/use-cases';
 
 describe('Money', () => {
-  it('Should return 10 when five dollars are multiplied by two and 15 when miltiplier by three', () => {
+  it('Should correctly handle dollar multiplication', () => {
     const five = new Dollar(5);
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
   });
-  it('Should return 10 when five euros are multiplied by two and 15 when miltiplier by three', () => {
+  it('Should correctly handle euro multiplication', () => {
     const five = new Euro(5);
     expect(five.times(2).equals(new Euro(10))).toBeTruthy();
     expect(five.times(3).equals(new Euro(15))).toBeTruthy();
   });
-  it('should return thre when dollars and euros of the same value are compared', () => {
+  it('should correctly handle money equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
     expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
