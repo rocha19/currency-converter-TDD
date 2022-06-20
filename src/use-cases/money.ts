117a92894ce5e93e2b557201a637ca4b582b0cad
rocha19 <rochaarqueologia@outlook.com>
2022-06-20 14:09:05 -0300

    feat: compare euros with dollars

--- src/use-cases/money.ts
+++ src/use-cases/money.ts
@@ -1,6 +1,9 @@
 export class Money {
   public amount: number;
   equals(other: Money) {
+    if (this.constructor !== other.constructor) {
+      return false;
+    }
     return this.amount === other.amount;
   }
 }
