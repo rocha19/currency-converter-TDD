3c8c12fb6d86896d2d10f41673d70860d6366553
rocha19 <rochaarqueologia@outlook.com>
2022-06-20 13:26:41 -0300

    test: refactor make amount private

--- test/use-cases/dollar.spec.ts
+++ test/use-cases/dollar.spec.ts
@@ -3,10 +3,8 @@ import { Dollar } from '@/use-cases';
 describe('Dollar', () => {
   it('Should return 10 when five dollars are multiplied by two', () => {
     const five = new Dollar(5);
-    let product: Dollar = five.times(2);
-    expect(product.amount).toEqual(10);
-    product = five.times(3);
-    expect(product.amount).toEqual(15);
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
   });
   it('should return thre when dollars of the same value are compared', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
