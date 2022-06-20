b0d8f4a40dcf3ae4cbf28f5c122a1155a74debd8
rocha19 <rochaarqueologia@outlook.com>
2022-06-20 13:45:44 -0300

    refactor: generalize to money

--- test/use-cases/euro.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Euro } from '@/use-cases';
-
-describe('Euro', () => {
-  it('Should return 10 when five euros are multiplied by two and 15 when miltiplier by three', () => {
-    const five = new Euro(5);
-    expect(five.times(2).equals(new Euro(10))).toBeTruthy();
-    expect(five.times(3).equals(new Euro(15))).toBeTruthy();
-  });
-});
