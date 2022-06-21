dbeaa9702b9856e33c5e8f35861b72abaa219053
rocha19 <rochaarqueologia@outlook.com>
2022-06-21 10:20:15 -0300

    test: handle conversion rates in the bank

--- test/use-cases/money.spec.ts
+++ test/use-cases/money.spec.ts
@@ -40,4 +40,13 @@ describe('Money', () => {
     const result: Money = bank.reduce(Money.dollar(1), 'USD')
     expect(result).toEqual(Money.dollar(1))
   })
+  it('should reduce from different currencies', () => {
+    const bank = new Bank()
+    bank.addRate('EUR', 'USD', 2)
+    const result = bank.reduce(Money.euro(2), 'USD')
+    expect(result).toEqual(Money.dollar(1))
+  })
+  it('should handle identity rate', () => {
+    expect(new Bank().rate('USD', 'USD')).toEqual(1)
+  });
 })
