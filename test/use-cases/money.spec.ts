ce0ce401838eff05a42a41e03bcdf94eae7b4b32
rocha19 <rochaarqueologia@outlook.com>
2022-06-21 09:53:40 -0300

    test: reduce from a sum...

--- test/use-cases/money.spec.ts
+++ test/use-cases/money.spec.ts
@@ -1,25 +1,43 @@
-import { Bank, Expression, Money } from '@/use-cases';
+import { Bank, Expression, Money, Sum } from '@/use-cases'
 
 describe('Money', () => {
   it('Should handle dollar multiplication', () => {
-    const five: Money = Money.dollar(5);
-    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
-  });
+    const five: Money = Money.dollar(5)
+    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy()
+    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
+  })
   it('should handle money equality', () => {
-    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
-    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
-    expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy();
-  });
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
+    expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
+  })
   it('should handle currencies', () => {
-    expect(Money.dollar(1).currency()).toEqual('USD');
-    expect(Money.euro(1).currency()).toEqual('EUR');
-  });
+    expect(Money.dollar(1).currency()).toEqual('USD')
+    expect(Money.euro(1).currency()).toEqual('EUR')
+  })
   it('should handle simple addition', () => {
-    const five = Money.dollar(5);
-    const sum: Expression = five.plus(five);
-    const bank = new Bank();
-    const reduced: Money = bank.reduce(sum, 'USD');
-    expect(reduced.equals(Money.dollar(10))).toBeTruthy();
-  });
-});
+    const five = Money.dollar(5)
+    const sum: Expression = five.plus(five)
+    const bank = new Bank()
+    const reduced: Money = bank.reduce(sum, 'USD')
+    expect(reduced.equals(Money.dollar(10))).toBeTruthy()
+  })
+  it('should return a sum when plus is used', () => {
+    const five = Money.dollar(5)
+    const result: Expression = five.plus(five)
+    const sum = result as Sum
+    expect(sum.augend).toEqual(five)
+    expect(sum.addend).toEqual(five)
+  })
+  it('should reduce a sum', () => {
+    const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4))
+    const bank = new Bank()
+    const result = bank.reduce(sum, 'USD')
+    expect(result).toEqual(Money.dollar(7))
+  })
+  it('should reduce from a money', () => {
+    const bank = new Bank()
+    const result: Money = bank.reduce(Money.dollar(1), 'USD')
+    expect(result).toEqual(Money.dollar(1))
+  })
+})
